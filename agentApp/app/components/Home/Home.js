d5a4224e9e7a2a0040524799d8f8da9ec4bac335
Gabriel K <kkkgabriel@hotmail.com>
2020-03-23 10:47:56 +0800

    Created a new file 'constants.js' for clean coding integrated chat page's logic and UI.  Now a 'start' keyword will trigger a pop up, and if accepted, both parties can chat

--- agentApp/app/components/Home/Home.js
+++ agentApp/app/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { Chat } from '@progress/kendo-react-conversational-ui';
 import { Dialog, DialogActionsBar } from '@progress/kendo-react-dialogs';
-
+import * as consts from '../constants.js';
 
 class Home extends Component {
 
@@ -22,7 +22,7 @@ class Home extends Component {
             conversation: {},
             visible:false,
             "isAvailable":false,
-            messages: [
+            messages: [		// for population of kendo
                 {
                     author: this.bot,
                     timestamp: new Date(),
@@ -31,38 +31,36 @@ class Home extends Component {
             ]
 
         }
-
-        this.login();
+		
         this.conversationsChangedHandler = this.conversationsChangedHandler.bind(this);
+        this.login();
         this.addNewMessage = this.addNewMessage.bind(this);
         this.sendToRainbow= this.sendToRainbow.bind(this);        
         this.toggleDialog = this.toggleDialog.bind(this);
         this.toggleisAvailable = this.toggleisAvailable.bind(this);
         this.reroute = this.reroute.bind(this);
         this.updateIncomingMessage = this.updateIncomingMessage.bind(this);
+		this.updateRainbowMessages = this.updateRainbowMessages.bind(this);
+		this.changeBot = this.changeBot.bind(this);
 
-        document.addEventListener(
-            rainbowSDK.conversations.RAINBOW_ONCONVERSATIONSCHANGED,
-            this.conversationsChangedHandler
-        );
     }
 
     conversationsChangedHandler(){
-        console.log("conversationsChangedHandler triggered"); 
-        // console.log(this); 
-        // console.log(this.state); 
+        console.log("conversationsChangedHandler triggered");
         // console.log(this.state.conversations);
-
         var conversations = this.state.conversations;
-        var conversation = this.findNewConversation(conversations);
-        this.setState({
-            conversation: conversation
-        });
+		if ( this.state.isAvailable ) {
+			this.updateRainbowMessages();
+		} else {
+			this.findNewConversation();
+		}
     }
     reroute(){
         console.log("this is rerouting");
     }
+
     toggleDialog() {
+		console.log("toggling dialog");
         this.setState({
             visible: !this.state.visible
         });
@@ -74,6 +72,7 @@ class Home extends Component {
             visible: !this.state.visible
         })
     }
+
     addNewMessage(event) {
         let myResponse = Object.assign({}, event.message);
         this.setState((prevState) => ({
@@ -85,15 +84,15 @@ class Home extends Component {
         this.sendToRainbow(event.message);
     };
 
-
-    sendToRainbow(question){
-        console.log("to add in rainbow send message here")
-        
+    sendToRainbow(msg){
+        // console.log("to add in rainbow send message here")
+        console.log(msg);
+		rainbowSDK.im.sendMessageToConversation(this.state.conversation, msg.text);
     }
 
     updateIncomingMessage(){
         let theirResponse = {
-            author: this.bot,
+            author: this.user,
             text: "update text here",
             timestamp: new Date()
         }
@@ -105,24 +104,54 @@ class Home extends Component {
         }))
     }
 
-    findNewConversation(conversations){
+	updateRainbowMessages(){	// display messages from rainbow conversation onto the kendo chat element
+		let lastMessage = this.state.conversation.messages[this.state.conversation.messages.length-1];
+		// console.log(lastMessage.side);
+		if ( lastMessage.side == "L" ){	// for rainbow, incoming messages are displayed on the left
+			let theirResponse = {
+				author: this.state.conversation.contact.loginEmail,
+				text: this.state.conversation.lastMessageText,
+				timestamp: new Date()
+			}
+			this.setState((prevState) => ({
+				messages: [
+					...prevState.messages,
+					theirResponse
+				]
+			}))
+		}
+	}
+
+	changeBot(name){
+		this.bot.name = name;
+	}
+
+    findNewConversation(){
+		let conversations = this.state.conversations;
         var i = 0;
         while (i < conversations.length ){
-            var l = conversations[i].messages.length;
             try {
-                if ( conversations[i].messages[l-1].data == "start" ){
+                if ( conversations[i].lastMessageText == consts.START_KEYWORD ){
+
+					//let lm = conversations[i].messages[conversations[i].messages.length-1];
                     console.log("start found");
+					this.setState({
+						conversation: conversations[i]
+					});
                     this.toggleDialog();
-                    alert(conversations[i].contact.loginEmail+" wants to talk to you");
-                    return conversations[i];
+					this.changeBot(conversations[i].name.value);
+					this.updateRainbowMessages();	// display messages from rainbow conversation onto the kendo chat element
+					break;
+					// i = conversations.length;	// break the loop
                 }
             } catch (error) {
-                console.log(error);
+                // console.log(error);
             }
             i++;
         }
-        return ['conversations are the same'];
+        return [];	// return empty conversation
     }
+
 
     login(){
         console.log("I m here");
@@ -139,8 +168,12 @@ class Home extends Component {
             that.setState({
                 conversations: conversations
             }, function(){
-                console.log("this is the callback");
-                console.log(that);
+                // console.log("this is the callback");
+                // console.log(that);
+				document.addEventListener(
+					rainbowSDK.conversations.RAINBOW_ONCONVERSATIONSCHANGED,
+					this.conversationsChangedHandler
+				);
             });
             console.log(that.state.conversations);
         })
@@ -156,7 +189,7 @@ class Home extends Component {
                 <button className="k-button" onClick={this.toggleDialog}>Open Dialog</button>
                 {this.state.visible && 
                 <Dialog title={"Please confirm"} onClose={this.toggleDialog}>
-                    <p style={{ margin: "25px", textAlign: "center" }}>{this.user["name"]} is trying to connect to you. Do you want to accept?</p>
+                    <p style={{ margin: "25px", textAlign: "center" }}>{this.bot["name"]} is trying to connect to you. Do you want to accept?</p>
                     <DialogActionsBar>
                         <button className="k-button" onClick={this.toggleDialog}>Decline</button>
                         <button className="k-button" onClick={this.toggleisAvailable}>Accept</button>
