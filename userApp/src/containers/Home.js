1c939b9ba50550a843775da6ee3d544ec6bf05d4
Gabriel K <kkkgabriel@hotmail.com>
2020-03-31 02:46:57 +0800

    Implemented log in, starting the conversation, wait for accept. REST API calls not yet implemented, hard coded credentials and IDs

--- userApp/src/containers/Home.js
+++ userApp/src/containers/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
 import "@progress/kendo-theme-default/dist/all.css";
 import { Chat } from '@progress/kendo-react-conversational-ui';
 import axios from 'axios';
 
-const dialogFlowBaseUrl = "http://localhost:6000"
+const dialogFlowBaseUrl = "http://localhost:3005"
+const START_KEYWORD = "start"
+const ACCEPT_KEYWORD = "accept"
+
 class Home extends React.Component{
     constructor(props) {
         super(props);
@@ -30,18 +33,23 @@ class Home extends React.Component{
                     console.log(res["data"]);
                 })
         this.state = {
-            //"version": rainbowSDK.version(),
             messages: [
                 {
                     author: this.bot,
                     timestamp: new Date(),
                     text: " Hello! Please enter a name in order to chat ",
                 }
-            ]
+            ],
+            conversation: '',
+            contact: ''
 		};
 		// This allows us to have event hadnler that handles this
 		this.addNewMessage = this.addNewMessage.bind(this);
 	}
+
+    componentDidMount(){
+        this.login();
+    }
 
     addNewMessage = (event) => {
 		let value = this.parseText(event);
@@ -56,6 +64,7 @@ class Home extends React.Component{
 			this.addBotMessage(event.message)
 		}
 		else{
+            // console.log(event);
 			this.addRainbowMessage(event.message)
 		}
 	};
@@ -73,66 +82,78 @@ class Home extends React.Component{
             if(messagecheck[messagecheck.length-1]==="problem?"){
                 newMessage.suggestedActions= [
                     {
-                    type: "reply",
-                    value: "Accounts and bills"
+                        type: "reply",
+                        value: "Accounts and bills"
                     },        
                     {
-                    type: "reply",
-                    value: "Broadband"
+                        type: "reply",
+                        value: "Broadband"
                     },
                     {
-                    type: "reply",
-                    value: "Home line"
+                        type: "reply",
+                        value: "Home line"
                     },
                     {
-                    type: "reply",
-                    value: "Mobile prepaid"
+                        type: "reply",
+                        value: "Mobile prepaid"
                     },
                     {
-                    type: "reply",
-                    value: "Mobile postpaid"
+                        type: "reply",
+                        value: "Mobile postpaid"
                     },
                     {
-                    type: "reply",
-                    value: "Online purchases"
+                        type: "reply",
+                        value: "Online purchases"
                     },
                     {
-                    type: "reply",
-                    value: "Singtel TV"
+                        type: "reply",
+                        value: "Singtel TV"
                     },
             
                 ];
             }
             if(messagecheck[messagecheck.length-1]==="right?"){
-            newMessage.suggestedActions= [
-                {
-                type: "reply",
-                value: "Yes"
-                },        
-                {
-                type: "reply",
-                value: "No"
-                }
-            ];
+                newMessage.suggestedActions= [
+                    {
+                        type: "reply",
+                        value: "Yes"
+                    },        
+                    {
+                        type: "reply",
+                        value: "No"
+                    }
+                ];
             }
             if(messagecheck[messagecheck.length-1]==="agent!"){
                 console.log("switching over to rainbow");
-                this.bot = { name: "rainbow agent", id: Date.now().toString() };
+                this.bot = { 
+                    name: "rainbow agent", 
+                    id: Date.now().toString()
+                };
+
+                // disable text input until connection with agent is established
+                this.disableTextInput(true);
+
+                // Add REST API call here to get the available agent ID
+                let agentId = "5e600991d8084c29e64eb436"; // Mobile postpaid
+
+                // establish connection
+                this.openConversationWithAgentId(agentId);
+
             }
             this.setState({
                 messages: [...this.state.messages, newMessage]
             });
-		}
-		)	
+		})	
 	}
 
 	addRainbowMessage = (message) =>{
-		//TODO: fill in rainbow api messages here.
+		window.rainbowSDK.im.sendMessageToConversation(this.state.conversation, message.text);
 		let newMessage = Object.assign({}, message);
-		newMessage.author = this.bot
-		newMessage.text = "this is the placeholder for rainbow"
+		newMessage.author = this.user
+		newMessage.text = message.text;
 		this.setState({
-		  messages: [...this.state.messages, newMessage]
+            messages: [...this.state.messages, newMessage]
 		});
 	
 	  }
@@ -147,6 +168,106 @@ class Home extends React.Component{
         }
     }
 
+    login = () => {
+
+        // Add REST API call here to replace the hardcoded username and pw
+        var rainbowLogin = "user1@singco.com";
+        var rainbowPassword = "Longpassword!1";
+
+        if (typeof window.rainbowSDK === 'undefined'){
+            return
+        }
+
+        // console.log("logging in");
+        window.rainbowSDK.connection.signin(rainbowLogin, rainbowPassword)
+            .then(account => {
+                console.log("Successful Login");
+                console.log(account);
+            })
+            .catch(err => {
+                console.log("failed to login")
+                console.log(err);
+            })
+    }
+
+    openConversationWithAgentId = (strId) =>{
+        let that = this;
+
+        // look for contact with id
+        window.rainbowSDK.contacts
+            .searchById(strId)
+            .then(function(contact){
+                console.log("found contact");
+                // console.log(contact)
+
+                that.setState({
+                    contact: contact
+                })
+
+                // look for conversation with contact
+                window.rainbowSDK.conversations  
+                .openConversationForContact(contact)
+                .then(function(conversation){
+                    console.log("conversation opened!");
+                    // console.log(conversation);
+                    that.setState({
+                        conversation: conversation
+                    })
+
+                    // start listening to conversation change
+                    that.addConversationListener();
+
+                    // send start keyword
+                    that.sendKeyword(START_KEYWORD);
+
+                })
+                .catch(function(err){
+                    console.log(err);
+                });
+            })
+            .catch(function(err){
+                console.log(err);
+            });
+
+    }
+
+    addConversationListener = () => {
+        document.addEventListener(
+            window.rainbowSDK.conversations.RAINBOW_ONCONVERSATIONCHANGED,
+            this.conversationChangedHandler
+        );
+    }
+
+    sendKeyword = (keyword) => {
+        window.rainbowSDK.im.sendMessageToConversation(this.state.conversation, keyword);
+    }
+
+
+    conversationChangedHandler = (convo) =>{
+        // get the last message
+        let lastMessage = this.state.conversation.lastMessageText;
+        // console.log(lastMessage)
+
+        // special case "accept" keyword
+        if ( lastMessage == ACCEPT_KEYWORD ){
+
+            // enabling the text input
+            this.disableTextInput(false);
+        }
+
+        // create message that is suitable for kendo chat to display
+        let newMessage = Object.assign({});
+        newMessage.text = lastMessage;
+        newMessage.author = this.bot;
+        this.setState({
+            messages: [...this.state.messages, newMessage]
+        });
+    }
+
+    disableTextInput = (bool) =>{
+        document.getElementsByClassName('k-input')[0].disabled = bool;
+    }
+
     reroute = () => {
 
     }
@@ -154,7 +275,8 @@ class Home extends React.Component{
     render() {       
         return (
             <div>
-                <h1>Conversational UI</h1>
+                <h1>Convsersational UI</h1>
+                <p>{this.state.version}</p>
                 <Chat user={this.user}
                     messages={this.state.messages}
                     onMessageSend={this.addNewMessage}
